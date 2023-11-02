8e148010d98fd5014d96eed2f8fade433d812075
29039 <61966298+29039@users.noreply.github.com>
2023-11-02 18:23:17 +1100

    Update crd-fixer.ps1

--- crd-fixer.ps1
+++ crd-fixer.ps1
@@ -9,50 +9,30 @@
 $enableLogging = $false
 $logFilePath = "C:\Path\To\Your\Log\CRDFixer_Log.txt"
 
-function Start-Logging {
-    if ($enableLogging) {
-        Start-Transcript -Path $logFilePath -Append
-        Write-Output "Script started at: $(Get-Date)"
-    }
+# Check if running as Administrator
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if (-not $isAdmin) {
+    Write-Host "Please relaunch as Admin to install" -ForegroundColor "Red"
+    Write-Host "Press any key to continue..." -ForegroundColor "Yellow"
+    Read-Host
+    Exit
 }
 
-function Stop-Logging {
-    if ($enableLogging) {
-        Write-Output "Script stopped at: $(Get-Date)"
-        Stop-Transcript
-    }
+# Start Logging
+if ($enableLogging) {
+    Start-Transcript -Path $logFilePath -Append
+    Write-Output "Script started at: $(Get-Date)"
 }
 
-function Log-Message {
-    param (
-        [string]$message,
-        [string]$color = "White"
-    )
+# Check if CRD Fixer task already exists
+$schedule = New-Object -ComObject Schedule.Service
+$schedule.Connect()
+$tasks = $schedule.GetFolder("\").GetTasks(0)
+$doesCRDFixerTaskExist = $tasks.Name -contains "CRD Fixer"
 
-    Write-Host $message -ForegroundColor $color
-
-    if ($enableLogging) {
-        $formattedMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $message"
-        Add-Content -Path $logFilePath -Value $formattedMessage
-    }
-}
-
-function Check-CRDFixerTask {
-    # Check if CRD Fixer task already exists
-    $schedule = New-Object -ComObject Schedule.Service
-    $schedule.Connect()
-    $tasks = $schedule.GetFolder("\").GetTasks(0)
-
-    foreach ($task in ($tasks | Select-Object Name)) {
-        if ($task.Name -eq "CRD Fixer") {
-            return $true
-        }
-    }
-    return $false
-}
-
-function Install-CRDFixerTask {
-    # Install the task if it doesn't exist
+# Install CRD Fixer task if it doesn't exist
+if (-not $doesCRDFixerTaskExist) {
     $action = New-ScheduledTaskAction -Execute powershell.exe -Argument "-ExecutionPolicy RemoteSigned ""`$crd='chromoting';if((Get-Service `$crd).StartType -ne 'Disabled'){Set-Service `$crd -StartupType Automatic;Start-Service `$crd}""" -WorkingDirectory %SystemRoot%
     $trigger = New-ScheduledTaskTrigger -Daily -At 6pm
     $description = "Sets Chrome Remote Desktop (chromoting service) back to Automatic startup"
@@ -63,39 +43,19 @@ function Install-CRDFixerTask {
     Set-ScheduledTask -TaskName "CRD Fixer" -Settings $settings 
     Start-ScheduledTask -TaskName "CRD Fixer"
 
-    Log-Message "CRD Fixer task installed."
-}
-
-# Check if running as Administrator
-$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-
-if (-not $isAdmin) {
-    # Relaunch as administrator
-    Log-Message "Please relaunch as Admin to install" -color "Red"
-    Log-Message "Press any key to continue..." -color "Yellow"
-    Read-Host
-    Exit
-}
-
-# Start Logging
-Start-Logging
-
-# Check if CRD Fixer task already exists
-$doesCRDFixerTaskExist = Check-CRDFixerTask
-
-# Install CRD Fixer task if it doesn't exist
-if (-not $doesCRDFixerTaskExist) {
-    Install-CRDFixerTask
+    Write-Host "CRD Fixer task installed."
 }
 
 # Provide user feedback
 if ($doesCRDFixerTaskExist) {
-    Log-Message "CRD Fixer already installed. Delete from Task Scheduler if you want to re-install." -color "Red"
+    Write-Host "CRD Fixer already installed. Delete from Task Scheduler if you want to re-install." -ForegroundColor "Red"
 } else {
-    Log-Message "CRD Fixer successfully installed." -color "Green"
+    Write-Host "CRD Fixer successfully installed." -ForegroundColor "Green"
 }
 
 # Stop Logging
-Stop-Logging
-
+if ($enableLogging) {
+    Write-Output "Script stopped at: $(Get-Date)"
+    Stop-Transcript
+}
 ##
